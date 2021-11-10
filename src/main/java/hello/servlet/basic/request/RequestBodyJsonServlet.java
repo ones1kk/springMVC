91618333f3b375a0681df954b5fba0a2a13c5e91
ones1kk <kwsic@naver.com>
2021-11-10 23:36:39 +0900

    ✨ 3rd-3 : Json

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,37 @@
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+@WebServlet(name = "requestJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    ServletInputStream inputStream = request.getInputStream();
+    String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+    System.out.println("MessageBody = " + messageBody);
+    System.out.println();
+
+    HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+    System.out.println("helloData.username = " + helloData.getUsername());
+    System.out.println("helloData.username = " + helloData.getAge());
+
+    response.getWriter().write("ok");
+  }
+
+}
