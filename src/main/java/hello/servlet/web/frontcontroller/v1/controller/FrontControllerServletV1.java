3fbd7b671659c30f71a465f5e1f6fda4032c5bfc
ones1kk <kwsic@naver.com>
2021-11-14 23:14:04 +0900

    ✨ 6th-1 : FrontController

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/FrontControllerServletV1.java
@@ -0,0 +1,38 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+  private final Map<String, ControllerV1> controllerV1Map = new HashMap<>();
+
+  public FrontControllerServletV1() {
+    controllerV1Map.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+    controllerV1Map.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+    controllerV1Map.put("/front-controller/v1/members", new MemberListControllerV1());
+  }
+
+  @Override
+  public void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    System.out.println("FrontControllerServletV1.service");
+
+    String requestURI = request.getRequestURI();
+    ControllerV1 controller = controllerV1Map.get(requestURI);
+    if (controller == null) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    controller.proccess(request, response);
+  }
+
+}
