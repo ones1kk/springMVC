963e16c87d516e89ecc040c9b5181fde524ec87b
ones1kk <kwsic@naver.com>
2021-11-16 22:32:48 +0900

    ✏️ Fix : Typos

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/FrontControllerServletV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/FrontControllerServletV3.java
@@ -53,7 +53,7 @@ public class FrontControllerServletV3 extends HttpServlet {
   private Map<String, String> createParamMap(HttpServletRequest request) {
     Map<String, String> paramMap = new HashMap<>();
     request.getParameterNames().asIterator()
-        .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter("username")));
+        .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
     return paramMap;
   }
 
