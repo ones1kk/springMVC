a234ad47fb0467f9b296ca79b771811b0518fef1
ones1kk <kwsic@naver.com>
2022-01-25 23:27:39 +0900

    🔥 Retry : Request Data(GET query parameter)

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -8,32 +8,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 1. 파라미터 전송 기능 https://localhost:8079/request-param?username=hello&age=20
+ * 1. 파라미터 전송 기능
+ * http://localhost:8079/request-param?username=hello&age=20
  */
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    System.out.println("[전체 파ㅣ라미터 조히] - start");
-    request.getParameterNames().asIterator().forEachRemaining(
-        paramName -> System.out.println(paramName + " : " + request.getParameter(paramName)));
-    System.out.println("[전체 파라미터 조히] - end");
-    System.out.println();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조히] - start");
+        request.getParameterNames().asIterator().forEachRemaining(
+            paramName -> System.out.println(paramName + " = " + request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조히] - end");
+        System.out.println();
+        System.out.println("[단일 파라미터 조히] - start");
+        String userName = request.getParameter("username");
+        System.out.println("name = " + userName);
+        System.out.println("[단일 파라미터 조히] - end");
+        System.out.println();
 
-    System.out.println("[단일 파라미터 조회} - start");
-    System.out.println("username : " + request.getParameter("username"));
-    System.out.println("age : " + request.getParameter("age"));
-    System.out.println("[단일 파라미터 조회} - end");
-    System.out.println();
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] names = request.getParameterValues("username");
+        for(String name : names) {
+            System.out.println("name = " + name);
+        }
 
-    // 중복 사용 지양
-    // 중복시 getParameterValues()의 첫 번쨰 값 return
-    String[] usernames = request.getParameterValues("username");
-    for (String name : usernames) {
-      System.out.println("username : " + name);
+        response.getWriter().write("ok");
     }
-    response.getWriter().write("OK");
-  }
 }
