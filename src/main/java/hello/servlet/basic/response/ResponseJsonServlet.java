bd6db1e366f8074c82fda532d57294c84680274f
ones1kk <kwsic@naver.com>
2022-01-26 00:12:26 +0900

    🔥 Retry : HttpServletResponse JSON

--- src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -3,6 +3,7 @@ package hello.servlet.basic.response;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.servlet.basic.HelloData;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,28 +13,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
 
-  private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    // Content-Type : application/json
-    response.setContentType("application/json");
-    response.setCharacterEncoding("utf-8");
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.setContentType("application/json");
+//        response.setCharacterEncoding("utf-8");
 
-    HelloData helloData = new HelloData();
-    helloData.setUsername("kim");
-    helloData.setAge(20);
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(10);
 
-    // {"username":"kim","age":20}
-    String result = objectMapper.writeValueAsString(helloData);
-    response.getWriter().write(result);
+        String result = objectMapper.writeValueAsString(helloData);
+//        response.getWriter().write(result);
+        response.getOutputStream().write(result.getBytes(StandardCharsets.UTF_8));
 
     /*
       application/json 은 스펙상 utf-8 형식을 사용하도록 정의되어 있다.
       response.getWriter()를 사용하면 추가 파라미터를 자동으로 추가해버린다. 이때는
       response.getOutputStream()으로 출력하면 그런 문제가 없다.
      */
-  }
+    }
 
 }
