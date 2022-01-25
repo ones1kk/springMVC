7158b36474d12568b0f63865225f15fcd18a7e86
ones1kk <kwsic@naver.com>
2022-01-25 23:36:13 +0900

    🔥 Retry : Request body String

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -13,15 +13,14 @@ import org.springframework.util.StreamUtils;
 @WebServlet(name = "RequestBodyStringServlet", urlPatterns = "/request-body-string")
 public class RequestBodyStringServlet extends HttpServlet {
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
-    // Content-Type : text/plain
-    ServletInputStream inputStream = request.getInputStream();
-    String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
 
-    System.out.println("messageBody = " + messageBody);
-    response.getWriter().write("ok");
-  }
+        response.getWriter().write("ok");
+    }
 }
