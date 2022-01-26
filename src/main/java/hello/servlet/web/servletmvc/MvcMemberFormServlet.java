3173d9641c0062361f46be66843b88eb4f850b73
ones1kk <kwsic@naver.com>
2022-01-26 23:00:35 +0900

    🔥 Retry : MVC Servlet

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -11,22 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
 public class MvcMemberFormServlet extends HttpServlet {
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    String viewPath = "/WEB-INF/views/new-form.jsp";
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
 
-    // option + command + v 변수 자동 추출
-    RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-    dispatcher.forward(request, response);
+        dispatcher.forward(request, response);
 
-    /*
-    WEB-INF : 외부에서 뷰 호출을 막음( WAS 룰임) : controller를 거치게 됨
+        /*
+        WEB-INF : 외부에서 뷰 호출을 막음( WAS 룰임) : controller를 거치게 됨
 
-    redirect : 실제 클라이언트(웹 브라우저)에 응답이 나갔다가, 클라이언트가 redirect 경로로 다시
-    요청한다. 따라서 클라이언트가 인지할 수 있고, URL 경로도 실제로 변경된다 (호출 2번)
+        redirect : 실제 클라이언트(웹 브라우저)에 응답이 나갔다가, 클라이언트가 redirect 경로로 다시
+        요청한다. 따라서 클라이언트가 인지할 수 있고, URL 경로도 실제로 변경된다 (호출 2번)
 
-    forward : 서버 내부에서 일어나는 호출이기 때문에 클라이언트가 전혀 인지하지 못한다.
-     */
-  }
+        forward : 서버 내부에서 일어나는 호출이기 때문에 클라이언트가 전혀 인지하지 못한다.
+         */
+    }
 }
