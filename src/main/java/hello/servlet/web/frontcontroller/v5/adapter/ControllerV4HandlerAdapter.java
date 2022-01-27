e19fd2b1750a8d2a2103c0bace02da76f5d1170c
ones1kk <kwsic@naver.com>
2022-01-28 00:04:14 +0900

    🔥 Retry : FrontController V52

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -20,10 +20,10 @@ public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws ServletException, IOException {
-        ControllerV4 controller = (ControllerV4) handler;
 
+        ControllerV4 controller = (ControllerV4) handler;
         Map<String, String> paramMap = createParamMap(request);
-        Map<String, Object> model = new HashMap<>();
+        HashMap<String, Object> model = new HashMap<>();
 
         String viewName = controller.process(paramMap, model);
 
